51e1d871e92c32d698bec1e7dcfa04ae4034b1c7
TetianaMaltseva <maltsevamaitsev@gmail.com>
2025-02-01 18:11:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerMenu = document.getElementById("burger-menu");
+    const navLinks = document.getElementById("nav-links");
+
+    burgerMenu.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
