7dd55fbbe7e881873cc4fb52f2ddea0709d379f3
TetianaMaltseva <maltsevamaitsev@gmail.com>
2025-02-07 14:32:11 +0100

    Changes

--- script.js
+++ script.js
@@ -11,63 +11,56 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", function () {
     let swiper = new Swiper(".mySwiper", {
         slidesPerView: 3, // Показываем 3 фото
-        spaceBetween: 20, // Отступы между фото
-
+        spaceBetween: 10, // Отступы между фото
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-        loop: true, // Бесконечная прокрутка
+        // loop: true, // Бесконечная прокрутка
+        // loopedSlides: 3,
     });
 
+            // carousel 2
     let swiper2 = new Swiper(".carousel-2", {
         slidesPerView: 3,
-        // spaceBetween: 10,
+        spaceBetween: 10,
         direction: 'horizontal',
         loop: true,
+        loopedSlides: 3,
+        autoHeight: true,
+        effect: 'coverflow',
+        coverflowEffect: {
+          rotate: -10,
+          slideShadows: false,
+        //   stretch: 10,
+        },
+
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 10
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 10
+            },
+            // when window width is >= 640px
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 10
+            }
+          },
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
-            dynamicBullets: true,
+             dynamicBullets: true,
         },
        
     })
+
+    
 });
 
-// carousel 2
-
-
-
-// const track = document.querySelector(".slider-track");
-// const thumb = document.querySelector(".scrollbar-thumb");
-// const slides = document.querySelectorAll(".slide");
-// const totalSlides = slides.length;
-// const slideWidth = 610; // Ширина слайда + отступ
-
-// let index = 0;
-
-// function moveSlides() {
-//     track.style.transform = `translateX(-${index * slideWidth}px)`;
-//     thumb.style.transform = `translateX(${(index / (totalSlides - 3)) * 200}px)`;
-// }
-
-// // Управление слайдерами по клику
-// track.addEventListener('wheel',(e) => {
-//     e.preventDefault();
-//     if (e.deltaY < 0) {
-//         index--;
-//     } else {
-//         index++;
-//     }
-//     handleInfiniteScroll();
-//     moveSlides();
-// });
-
-// function handleInfiniteScroll() {
-//     if (index >= totalSlides) {
-//         index = 0;
-//     } else if (index < 0) {
-//         index = totalSlides - 1;
-//     }
-// }
 
